64028e8e2c8d18dd132599020dd1ab275d08546f
MasterkinG32 <lichwow_masterking@yahoo.com>
2021-04-30 10:54:09 +0430

    Unlimited moneys for jobs

--- server/paycheck.lua
+++ server/paycheck.lua
@@ -14,16 +14,16 @@ ESX.StartPayCheck = function()
 				elseif Config.EnableSocietyPayouts then -- possibly a society
 					TriggerEvent('esx_society:getSociety', xPlayer.job.name, function (society)
 						if society ~= nil then -- verified society
-							TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function (account)
-								if account.money >= salary then -- does the society money to pay its employees?
+							--TriggerEvent('esx_addonaccount:getSharedAccount', society.account, function (account)
+								--if account.money >= salary then -- does the society money to pay its employees?
 									xPlayer.addAccountMoney('bank', salary)
-									account.removeMoney(salary)
+									--account.removeMoney(salary)
 
 									TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary), 'CHAR_BANK_MAZE', 9)
-								else
-									TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), '', _U('company_nomoney'), 'CHAR_BANK_MAZE', 1)
-								end
-							end)
+								--else
+								--	TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), '', _U('company_nomoney'), 'CHAR_BANK_MAZE', 1)
+								--end
+							--end)
 						else -- not a society
 							xPlayer.addAccountMoney('bank', salary)
 							TriggerClientEvent('esx:showAdvancedNotification', xPlayer.source, _U('bank'), _U('received_paycheck'), _U('received_salary', salary), 'CHAR_BANK_MAZE', 9)
